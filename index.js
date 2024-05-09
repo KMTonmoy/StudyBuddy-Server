17b3d71a4a317368191f66f41a0f2f0034668c5f
KMTonmoy <tonmoyahamed2009@gmail.com>
2024-05-09 19:54:37 +0600

    MongoDB Connected

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+//Must remove "/" from your production URL
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "https://cardoctor-bd.web.app",
+            "https://cardoctor-bd.firebaseapp.com",
+        ],
+        credentials: true,
+    })
+);
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.syw0uxl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+
+        // await client.db("admin").command({ ping: 1 });
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('GroupGrid Study Room Server Is Running')
+})
+
+app.listen(port, () => {
+    console.log(`GroupGrid Server Is Running on port ${port}`)
+})
