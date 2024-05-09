7e6dcbd76e90eeea2a020af9508434ffd268775a
KMTonmoy <tonmoyahamed2009@gmail.com>
2024-05-09 20:15:40 +0600

    JWT Function Compleat

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-require('dotenv').config();
-const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
-//Must remove "/" from your production URL
-app.use(
-    cors({
-        origin: [
-            "http://localhost:5173",
-            "http://localhost:5174",
-        ],
-        credentials: true,
-    })
-);
-app.use(express.json());
+const express = require('express')
+const cors = require('cors')
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+require('dotenv').config()
+const port = process.env.PORT || 9000
+const app = express()
+
+const corsOptions = {
+    origin: [
+        'http://localhost:5173',
+        'http://localhost:5174',
+        'https://solosphere.web.app',
+    ],
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
+ 
+const verifyToken = (req, res, next) => {
+    const token = req.cookies?.token
+    if (!token) return res.status(401).send({ message: 'unauthorized access' })
+    if (token) {
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+            if (err) {
+                console.log(err)
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
+            console.log(decoded)
+
+            req.user = decoded
+            next()
+        })
+    }
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.syw0uxl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -35,6 +56,38 @@ async function run() {
         // Send a ping to confirm a successful connection
 
         // await client.db("admin").command({ ping: 1 });
+
+
+
+        // jwt generate
+        app.post('/jwt', async (req, res) => {
+            const email = req.body
+            const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '365d',
+            }).
+                res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === 'production',
+                    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                })
+                .send({ success: true })
+        })
+
+        // Clear token on logout
+        app.get('/logout', (req, res) => {
+            res
+                .clearCookie('token', {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === 'production',
+                    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                    maxAge: 0,
+                })
+                .send({ success: true })
+        })
+
+
+
 
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
