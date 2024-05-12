a1f03abe106d1aed4c7abd0f8ab3e7d55a3df4fc
KMTonmoy <tonmoyahamed2009@gmail.com>
2024-05-12 11:35:48 +0600

    Assignment Update Function Added

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 const express = require('express');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+const corsOptions = {
+    origin: [
+        'http://localhost:5173',
+        'http://localhost:5174',
+        'http://localhost:5175',
+
+    ],
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
 
 
+const verifyToken = (req, res, next) => {
+    const token = req.cookies?.token
+    if (!token) return res.status(401).send({ message: 'unauthorized access' })
+    if (token) {
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+            if (err) {
+                console.log(err)
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
+            console.log(decoded)
+
+            req.user = decoded
+            next()
+        })
+    }
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.syw0uxl.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -28,6 +55,39 @@ async function run() {
         // await client.connect();
         const assignmentCollection = client.db('GroupGrid').collection('assignments');
         const TakeAssignmnetCollection = client.db('GroupGrid').collection('takeAssignemnt')
+
+
+
+        // Jwt Start Here
+        app.post('/jwt', async (req, res) => {
+            const email = req.body
+            const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '365d',
+            }).
+                res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === 'production',
+                    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                })
+                .send({ success: true })
+        })
+
+
+
+        // Clear Web Token On Logout
+        app.get('/logout', (req, res) => {
+            res.clearCookie('token', {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                maxAge: 0,
+            }).send({ success: true });
+        });
+
+
+
+
         app.get('/assignment', async (req, res) => {
             const cursor = assignmentCollection.find();
             const result = await cursor.toArray();
@@ -42,6 +102,7 @@ async function run() {
             res.send(result);
 
         });
+
 
 
 
@@ -75,19 +136,40 @@ async function run() {
         });
 
 
- 
+
         app.put('/submited/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) }
             const options = { upsert: true };
             const updateMark = req.body;
-            const Mark = { 
+            const Mark = {
                 $set: {
                     result: updateMark.result,
                     feedback: updateMark.feedback,
                 }
             }
             const result = await TakeAssignmnetCollection.updateOne(filter, Mark, options);
+            res.send(result);
+        })
+
+
+
+        app.put('/assignment/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true };
+            const updateAssignment = req.body;
+            const assign = {
+                $set: {
+                    title: updateAssignment.title,
+                    thumbnailURL: updateAssignment.thumbnailURL,
+                    mark: updateAssignment.mark,
+                    difficultyLevel: updateAssignment.difficultyLevel,
+                    description: updateAssignment.description,
+                    dueDate: updateAssignment.dueDate,
+                }
+            }
+            const result = await assignmentCollection.updateOne(filter, assign, options);
             res.send(result);
         })
 
@@ -106,15 +188,6 @@ async function run() {
             const result = await assignmentCollection.deleteOne(query);
             res.send({ deletedCount: result.deletedCount });
 
-        });
-
-        app.get('/logout', (req, res) => {
-            res.clearCookie('token', {
-                httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',
-                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-                maxAge: 0,
-            }).send({ success: true });
         });
 
 
